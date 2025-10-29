fdf8cda41e552f31d107db610ba03d38d08e4193
memsbdm <mehmetbdm@outlook.fr>
2025-10-29 15:19:56 +0400

    add flash plugin

--- .wezterm.lua
+++ .wezterm.lua
@@ -21,4 +21,8 @@ local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
 config.colors = theme.colors()
 
 -- and finally, return the configuration to wezterm
+config.keys = {
+  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+}
+
 return config
